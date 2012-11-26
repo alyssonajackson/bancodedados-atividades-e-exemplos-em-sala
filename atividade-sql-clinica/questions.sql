81875a8e01ae79576f1110d2e711a1b49431107f
Alysson Ajackson <alysson@alysson.net>
2012-11-26 12:14:25 -0200

    answer question2 -> illustrate with tables (SQL RESULT)

--- atividade-sql-clinica/questions.sql
+++ atividade-sql-clinica/questions.sql
@@ -1,7 +1,33 @@
 -- 1. Buscar o nome e o CPF dos médicos com menos de 40 anos ou com especialidade diferente de traumatologia;
-SELECT * FROM `medico` WHERE `idade` < 40 AND `especialidade` <> 'Traumatologia';
+    SELECT * FROM `medico` WHERE `idade` < 40 AND `especialidade` <> 'Traumatologia';
+
+    /* O resultado deve ser algo parecido com isso:
+
+    +----------+--------+-------+---------------+-------------+---------------+---------------+
+    | idMedico | nome   | idade | especialidade | CPF         | cidade        | idAmbulatorio |
+    +----------+--------+-------+---------------+-------------+---------------+---------------+
+    |        6 | Carlos |    28 | Ortopedia     | 11000110000 | Ipatinga      |             5 |
+    |        7 | Marcia |    33 | Neurologia    | 11000111000 | Cataguaes     |             3 |
+    |        8 | Renato |    29 | Cardiologista | 10000110001 | Montes Claros |             4 |
+    +----------+--------+-------+---------------+-------------+---------------+---------------+
+
+    */
+
 
 -- 2.  Buscar o nome e a idade dos pacientes que não residem em Montes Claros;
+    SELECT `nome`, `idade` FROM medico WHERE `cidade` <> 'Montes Claros';
+
+    /* O resultado deve ser algo parecido com isso:
+        +--------+-------+
+        | nome   | idade |
+        +--------+-------+
+        | Maria  |    42 |
+        | Pedro  |    51 |
+        | Carlos |    28 |
+        | Marcia |    33 |
+        +--------+-------+
+    */
+
 -- 3.  Buscar o nome e a idade (em meses) dos pacientes;
 -- 4.  Em quais cidades residem os funcionários?
 -- 5.  Qual o menor e o maior salário dos funcionários residentes em Montes Claros?
