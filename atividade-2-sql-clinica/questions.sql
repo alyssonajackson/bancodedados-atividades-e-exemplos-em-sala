ac04c874b0c50c2fa744765f237d62ee09728447
Alysson Ajackson <alysson@alysson.net>
2012-12-05 12:54:53 -0200

    atividade 2 -> question 7

--- atividade-2-sql-clinica/questions.sql
+++ atividade-2-sql-clinica/questions.sql
@@ -9,8 +9,8 @@
         | Ana Paula     |
         | Paulo Freitas |
         | Lucia Ramos   |
-        | Carlos Jos�   |
-        | Maria Jos�    |
+        | Carlos José   |
+        | Maria José    |
         +---------------+
     */
 
@@ -66,3 +66,56 @@
         |             3 |             1 |
         +---------------+---------------+
     */
+
+-- 5. Quais as doenças diagnosticadas nas consultas?
+
+    SELECT DISTINCT `Doenca` FROM `consulta`;
+
+    /*
+        +---------------+
+        | Doenca        |
+        +---------------+
+        | fratura perna |
+        | fratura braco |
+        | traumatismo   |
+        | pneumonia     |
+        | consulta      |
+        | enxaqueca     |
+        | dor no peito  |
+        | arritimia     |
+        +---------------+
+    */
+
+-- 6. Qual o medico mais novo que atendeu algum paciente?
+
+    SELECT `M`.`nome`, `M`.`idade`, `M`.`idMedico` FROM `medico` `M` INNER JOIN `consulta` `C` ON `M`.`idMedico` = `C`.`idMedico` ORDER BY `M`.`idade` ASC LIMIT 1;
+
+    /*
+        +--------+-------+----------+
+        | nome   | idade | idMedico |
+        +--------+-------+----------+
+        | Carlos |    28 |        6 |
+        +--------+-------+----------+
+    */
+
+
+-- 7. Qual o medico que mais atendeu pacientes?
+
+    SELECT `C`.`idMedico`, COUNT(*) AS `total_consultas` FROM `consulta` `C` GROUP BY `C`.`idMedico` ORDER BY `total_consultas` DESC LIMIT 1;
+
+    -- Para obter também o nome do médico (e não simplesmente o ID):
+        -- SELECT `M`.`nome`, `C`.`idMedico`, COUNT(*) AS `total_consultas` FROM `consulta` `C` INNER JOIN `medico` `M` ON `C`.`idMedico` = `M`.`idMedico` GROUP BY `C`.`idMedico` ORDER BY `total_consultas` DESC LIMIT 1;
+
+    /*
+        +----------+-----------------+
+        | idMedico | total_consultas |
+        +----------+-----------------+
+        |        2 |               3 |
+        +----------+-----------------+
+    */
+
+
+-- 8. Listar o nome e idade de todos os pacientes que realizaram alguma consulta nos meses de outubro e novembro de 2010, ordenados por idade, do mais novo para o mais velho.
+-- 9. Qual a doença que mais foi diagnosticada durante as consultas? Quais médicos a diagnosticaram?
+-- 10. Listar as especialidades dos médicos que mais atenderam aos pacientes.
+-- 11. Qual ambulatório prestou o maior número de atendimentos?
